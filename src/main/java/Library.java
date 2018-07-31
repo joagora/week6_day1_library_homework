85c220e98751990320a92391d79bd14a1f4b9198
Joanna Gora <joanna.gora@protonmail.com>
2018-07-31 20:37:15 +0100

    created and tested getBookCollection method

--- src/main/java/Library.java
+++ src/main/java/Library.java
@@ -1,2 +1,14 @@
+import java.util.ArrayList;
+
 public class Library {
+
+    private ArrayList<Book> bookCollection;
+
+    public Library(){
+        this.bookCollection = new ArrayList<Book>();
+    }
+
+    public int getBookCollectionCount(){
+        return this.bookCollection.size();
+    }
 }
--- src/test/java/LibraryTest.java
+++ src/test/java/LibraryTest.java
@@ -1,2 +1,18 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class LibraryTest {
+
+    Library library1;
+    @Before
+    public void before(){
+        library1 = new Library();
+    }
+
+    @Test
+    public void canGetBookCollectionCount(){
+        assertEquals(0, library1.getBookCollectionCount());
+    }
 }
